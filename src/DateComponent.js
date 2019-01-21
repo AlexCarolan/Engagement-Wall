6dd0d55f580ae7818e9234ac5f3c75282d0a90b4
Alex Carolan <alexcarolan@dyn-10-32-191-103.winet.local>
2019-01-21 23:42:08 +0000

    Added all relevent values to date/time tracker

--- src/DateComponent.js
+++ src/DateComponent.js
@@ -2,20 +2,25 @@
 
 class CurrentDate extends React.Component {
 
-  render() {
+	pad(d) {
+    return (d < 10) ? '0' + d.toString() : d.toString();
+	}
+
+  	render() {
 		let date = new Date();
 		let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 		var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 		
 		let year = date.getFullYear();
 		let dayString = days[date.getDay()];
+		let day = date.getDate();
 		let month = months[date.getMonth()];
 		
 		let seconds = date.getSeconds();
 		let minutes = date.getMinutes();
 		let hours = date.getHours();
 		
-		return(<h2>{seconds}</h2>);
+		return(<h2>{`${this.pad(hours)}:${this.pad(minutes)}:${this.pad(seconds)} ${dayString} ${day} ${month} ${year}`}</h2>);
 		
   }
 	
