c854f29d7e73b9cc6b852bb72bf309692a858b25
Alex Carolan <alexcarolan@dyn-10-32-177-158.winet.local>
2019-01-24 02:01:42 +0000

    added additional formatting

--- src/OccupancyChart.js
+++ src/OccupancyChart.js
@@ -12,12 +12,20 @@
 
         var options = {
           backgroundColor: '#e5e7ea',
-          legend: { position: 'bottom' },
+          chartArea: {
+            top: 12,
+            right: 12,
+            bottom: 40,
+            left: 24,
+            height: '100%',
+            width: '100%'
+          },
+          legend: { position: 'none' },
           series: {
             0: { color: '#e2431e' }
           },
           hAxis: {title: 'Time of Day',  titleTextStyle: {color: '#000000'}, minValue: 0, maxValue: 24},
-          vAxis: {minValue: 0}
+          vAxis: {minValue:0, maxValue: 24}
         };
 
         var chart = new google.visualization.LineChart(document.getElementById('chart_occ'));
