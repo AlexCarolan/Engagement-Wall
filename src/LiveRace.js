3875647f590a345e474ce4f45dbf0271bfe3ca7a
Alex Carolan <alexcarolan@dyn-10-32-177-255.winet.local>
2019-02-12 00:13:48 +0000

    added animation to live race text

--- src/LiveRace.js
+++ src/LiveRace.js
@@ -1,9 +1,37 @@
 class Race extends React.Component { 
 
+	constructor(props) {
+      super(props);
+	      this.state = {
+	        currentEnd: 0,
+	        endings: ['', '.', '..', '...'],
+	        end: '.'
+	      };
+	}
+
+	componentDidMount() {
+		this.timerID = setInterval(
+	  		() => this.tick(),
+	  		1000
+		);
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.timerID);
+	}
+
+	tick() {
+		this.state.currentEnd++;
+		if(this.state.currentEnd >= 4){this.state.currentEnd = 0}
+		this.setState({
+			end: this.state.endings[this.state.currentEnd]
+		});
+	}
+
 	render() {
 		return(
 				<div className="textContainer">
-					<p id="raceText"> A new race will begin soon...</p>
+					<p id="raceText">{`A new race will begin soon${this.state.end}`}</p>
 				</div>
 			);
 	}
